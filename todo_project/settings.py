56102c90cb8cd8af988ffa7eea3ec3ac23386b35
Zeynep Akbalik <zeynep.akbalik@dorakholding.com.tr>
2022-01-14 14:35:14 +0300

    heroku settings edited

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -161,7 +161,7 @@ else:
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
     # DEFAULT_FILE_STORAGE = 'resume.custom_storages.MediaStorage'
-    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # xxxx
     AWS_S3_REGION_NAME = 'eu-central-1'
 
